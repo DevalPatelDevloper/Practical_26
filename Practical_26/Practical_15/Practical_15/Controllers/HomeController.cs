283eae66f669a7df8b027fd0b016fae9e199332d
SF-CPU-311\deval <deval.p@simformsolutions.com>
2022-04-06 11:29:39 +0530

    Convert to .net 5 to .net 6(practical_26)

---
+++ Practical_26/Practical_15/Practical_15/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practical_15.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+		private readonly ILogger _logger;
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+		[HttpGet]
+		public IActionResult Get()
+		{
+			_logger.LogInformation("Homepage was requested");
+			try
+			{
+				throw new Exception("Oops! An Exception is going on!");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex.ToString());
+			}
+			return Ok("Hello World APi");
+		}
+	}
+}
